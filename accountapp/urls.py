c06c90b110efc8095c34544d1647efc2b66ec47f
jiwonyoon <yjw5207@naver.com>
2021-03-23 15:16:47 +0900

    29

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name="accountapp/login.html"), name="login"),
     path('logout/', LogoutView.as_view(), name="logout"),
     path('create/', AccountCreateView.as_view(), name="create"),
-    path('detail/<int:pk>', AccountDetailView.as_view(), name="detail"),
-    path('update/<int:pk>', AccountUpdateView.as_view(), name="update"),
-    path('delete/<int:pk>', AccountDeleteView.as_view(), name="delete"),
+    path('detail/<int:pk>/', AccountDetailView.as_view(), name="detail"),
+    path('update/<int:pk>/', AccountUpdateView.as_view(), name="update"),
+    path('delete/<int:pk>/', AccountDeleteView.as_view(), name="delete"),
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
@@ -5,28 +6,35 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView, DetailView, DeleteView
 
+from accountapp.decorations import account_ownership_required
 from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
 
+has_ownership = [
+    account_ownership_required,
+    login_required,
+]
 
+@login_required
 def hello_world(request):
 
-    if request.method == "POST":
+        if request.method == "POST":
 
-        temp = request.POST.get('hello_world_input')
+            temp = request.POST.get('hello_world_input')
 
-        new_hello_world = HelloWorld()
-        new_hello_world.text = temp
-        new_hello_world.save()
+            new_hello_world = HelloWorld()
+            new_hello_world.text = temp
+            new_hello_world.save()
 
-        hello_world_list = HelloWorld.objects.all()
+            hello_world_list = HelloWorld.objects.all()
 
-        return HttpResponseRedirect(reverse('accountapp:hello_world'))
-    else:
-        hello_world_list = HelloWorld.objects.all()
-        return render(request, 'accountapp/hello_world.html', context={ 'hello_world_list': hello_world_list})
+            return HttpResponseRedirect(reverse('accountapp:hello_world'))
+        else:
+            hello_world_list = HelloWorld.objects.all()
+            return render(request, 'accountapp/hello_world.html', context={ 'hello_world_list': hello_world_list})
 
 
 class AccountCreateView(CreateView):
@@ -40,13 +48,18 @@ class AccountDetailView(DetailView):
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
 
-class AccountUpdateView(CreateView):
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
+class AccountUpdateView(UpdateView):
     model = User
     context_object_name = 'target_user'
     form_class = AccountUpdateForm  # form
     success_url = reverse_lazy('accountapp:hello_world')  # 클래스에선 lazy 추가
     template_name = 'accountapp/update.html'
 
+
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
