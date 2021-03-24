d1a6da2e62da53b2f95279fefe6f511b6cb69529
jiwonyoon <yjw5207@naver.com>
2021-03-24 17:30:57 +0900

    42

--- pinterest_prj/settings.py
+++ pinterest_prj/settings.py
@@ -48,12 +48,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'bootstrap4',
     'accountapp',
     'profileapp',
     'articleapp',
     'commentapp',
     'projectapp',
+    'subscribeapp',
+    'likeapp',
 ]
 
 MIDDLEWARE = [
